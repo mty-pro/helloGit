7d745dc1b42efb98a272252f8717998ef3549414
mty-pro <1641284478@qq.com>
2020-11-02 17:42:41 +0800

    主干添加

--- HelloGit/src/main/java/org/example/HelloGit.java
+++ HelloGit/src/main/java/org/example/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!更新1");
         System.out.println("Hello,Git!创建分支");
+        System.out.println("Hello,Git!主干添加");
     }
 }
