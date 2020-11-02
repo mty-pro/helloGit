4b68b035ff21c1e216316e72a868a5de4f49666f
mty-pro <1641284478@qq.com>
2020-11-02 17:17:14 +0800

    创建分支

--- HelloGit/src/main/java/org/example/HelloGit.java
+++ HelloGit/src/main/java/org/example/HelloGit.java
@@ -3,5 +3,6 @@ package org.example;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!更新1");
+        System.out.println("Hello,Git!创建分支");
     }
 }
