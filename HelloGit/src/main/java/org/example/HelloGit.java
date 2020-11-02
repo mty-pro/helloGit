4636026635a6f5fbb97cb458b0abddf5306c7f7a
mty-pro <1641284478@qq.com>
2020-11-02 20:14:09 +0800

    SSH添加

--- HelloGit/src/main/java/org/example/HelloGit.java
+++ HelloGit/src/main/java/org/example/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
         System.out.println("Hello,Git!更新本地库");
         System.out.println("Hello,Git!在线更新");
         System.out.println("Hello,Git!克隆更新");
+        System.out.println("Hello,Git!SSH更新");
 
     }
 }
